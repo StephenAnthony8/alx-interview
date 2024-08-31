11c94901d8bcbe56c5f417fd896ebe45d16a1211
StephenAnthony8 <anthonysteve02@gmail.com>
2024-09-01 00:03:14 +0300

    Backtracking algorithm implemented in N-queens question

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -81,12 +81,14 @@ def main():
         try:
             N = int(sys.argv[1])
         except Exception:
-            sys.exit("N must be a number")
+            print("N must be a number")
+            sys.exit(1)
 
         if (N >= 4):
             Nqueens(N)
         else:
-            sys.exit("N must be at least 4")
+            print("N must be at least 4")
+            sys.exit(1)
 
     else:
         print('Usage: nqueens N')
